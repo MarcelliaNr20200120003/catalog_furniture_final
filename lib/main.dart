dcf8e4bfb2a3aa9e922ffdbf237fbe021b7ac1a2
MarcelliaNr <marcellianr1234@gmail.com>
2023-02-21 23:45:08 +0700

    main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:catalog_furniture_final/app/controller/controllers/auth_controller_controller.dart';
+import 'package:catalog_furniture_final/app/modules/home/views/home_view.dart';
+import 'package:catalog_furniture_final/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -10,39 +12,75 @@ import 'app/routes/app_pages.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-      options: FirebaseOptions(
-          apiKey: '...',
-          appId: '...',
-          messagingSenderId: '...',
-          projectId: '...'));
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  final authC = Get.put(AuthControllerController(), permanent: true);
-
-  @override
-  Widget build(BuildContext context) {
-    const MaterialApp(
-      debugShowCheckedModeBanner: false,
-    );
-    return StreamBuilder(
-      stream: authC.streamAuthStatus,
+    // name: 'Catalog Furniture',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  Get.put(AuthControllerController(), permanent: true);
+  runApp(
+    StreamBuilder<User?>(
       builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.active) {
-          print(snapshot.data);
-          return GetMaterialApp(
-            title: 'Application',
-            initialRoute:
-                snapshot.data != null && snapshot.data!.emailVerified == true
-                    ? Routes.HOME
-                    : Routes.WELCOME,
-            getPages: AppPages.routes,
-          );
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
         }
-        return CircularProgressIndicator();
+        return GetMaterialApp(
+          title: "My Task Management",
+          initialRoute: snapshot.data != null ? Routes.HOME : Routes.WELCOME,
+          getPages: AppPages.routes,
+          debugShowCheckedModeBanner: false,
+        );
       },
-      // builder: authC.streamAuthStatus,
-    );
-  }
+    ),
+  );
 }
+
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+//   // Get.put(AuthControllerController(), permanent: true);
+//   runApp(MyApp()
+//       // StreamBuilder<User?>(
+//       // builder: (context, snapshot) {
+//       //   if (snapshot.connectionState == ConnectionState.waiting) {
+//       //     return const Center(child: CircularProgressIndicator());
+//       //   }
+//       // return
+//       //     GetMaterialApp(
+//       //   title: "My Task Management",
+//       //   initialRoute: Routes.WELCOME,
+//       //   getPages: AppPages.routes,
+//       //   debugShowCheckedModeBanner: false,
+//       // )
+//       //     // },
+//       //     // )
+//       );
+// }
+
+// class MyApp extends StatelessWidget {
+//   final authC = Get.put(AuthControllerController(), permanent: true);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     const MaterialApp();
+//     return StreamBuilder(
+//       stream: authC.streamAuthStatus,
+//       builder: (context, snapshot) {
+//         if (snapshot.connectionState == ConnectionState.active) {
+//           print(snapshot.data);
+//           return GetMaterialApp(
+//             debugShowCheckedModeBanner: false,
+//             title: 'Application',
+//             initialRoute:
+//                 snapshot.data != null && snapshot.data!.emailVerified == true
+//                     ? Routes.HOME
+//                     : Routes.WELCOME,
+//             getPages: AppPages.routes,
+//           );
+//         }
+//         return CircularProgressIndicator();
+//       },
+//       // builder: authC.streamAuthStatus,
+//     );
+//   }
+// }
