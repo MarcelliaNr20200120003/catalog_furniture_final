4e8d640699f8764b6da8fd2e89055af413121700
MarcelliaNr <marcellianr1234@gmail.com>
2023-02-21 23:45:28 +0700

    auth controller

--- lib/app/controller/controllers/auth_controller_controller.dart
+++ lib/app/controller/controllers/auth_controller_controller.dart
@@ -1,8 +1,75 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:catalog_furniture_final/app/routes/app_pages.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthControllerController extends GetxController {
-  FirebaseAuth auth = FirebaseAuth.instance;
+  // FirebaseAuth auth = FirebaseAuth.instance;
   Stream<User?> get streamAuthStatus => auth.authStateChanges();
+
+  UserCredential? _userCredential;
+  FirebaseAuth auth = FirebaseAuth.instance;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  Future signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    print(googleUser!.email);
+    // Once signed in, return the UserCredential
+    await auth
+        .signInWithCredential(credential)
+        .then((value) => _userCredential = value);
+
+    //firebase
+    CollectionReference users = firestore.collection('users');
+
+    final cekUsers = await users.doc(googleUser.email).get();
+    if (!cekUsers.exists) {
+      users.doc(googleUser.email).set({
+        'uid': _userCredential!.user!.uid,
+        'name': googleUser.displayName,
+        'email': googleUser.email,
+        'photo': googleUser.photoUrl,
+        'createdAt': _userCredential!.user!.metadata.creationTime.toString(),
+        'lastLoginAt':
+            _userCredential!.user!.metadata.lastSignInTime.toString(),
+        // 'list_cari':,
+      }).then((value) async {
+        try {
+          String temp = '';
+          for (var i = 0; i < googleUser.displayName!.length; i++) {
+            temp = temp + googleUser.displayName![i];
+            users.doc(googleUser.email).set({
+              await 'list_cari': FieldValue.arrayUnion([temp.toUpperCase()])
+            }, SetOptions(merge: true));
+          }
+        } catch (e) {
+          print(e);
+        }
+      });
+    } else {
+      users.doc(googleUser.email).update({
+        'lastLoginAt': _userCredential!.user!.metadata.lastSignInTime,
+      });
+    }
+    Get.offAllNamed(Routes.HOME);
+  }
+
+  Future logout() async {
+    await auth.signOut();
+    await GoogleSignIn().signOut();
+    Get.offAllNamed(Routes.LOGIN);
+  }
 }
