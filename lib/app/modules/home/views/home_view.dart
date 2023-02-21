3e8383915b3dde2def9dc465ddf64951757798ba
MarcelliaNr <marcellianr1234@gmail.com>
2023-02-21 23:46:08 +0700

    home view

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,10 +1,12 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
+// import 'package:google_fonts/google_fonts.dart';
 
 import '../../category_list/views/category_list_view.dart';
+import '../../constants/views/constants_view.dart';
 import '../../detail_screen/views/detail_screen_view.dart';
 import '../../product/views/product_view.dart';
 import '../../product_card/views/product_card_view.dart';
@@ -23,64 +25,203 @@ class HomeView extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     List<int> list = [1, 2, 3, 4, 5, 6];
-    return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          bottom: false,
-          child: Column(
-            children: <Widget>[
-              SearchBox(
-                onChanged: (value) {},
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          children: <Widget>[
+            SearchBox(
+              onChanged: (value) {},
+            ),
+            Container(
+              color: Colors.white,
+              height: 220,
+              width: double.infinity,
+              child: CarouselSlider(
+                options: CarouselOptions(),
+                items: imageSliders,
               ),
-              Container(
-                height: 220,
-                width: double.infinity,
-                child: CarouselSlider(
-                  options: CarouselOptions(),
-                  items: imageSliders,
-                ),
-              ),
-              CategoryList(),
-              // ignore: prefer_const_constructors
-              SizedBox(height: 20.0 / 2),
-              Expanded(
-                child: Stack(
-                  children: <Widget>[
-                    // Our background
-                    Container(
-                      margin: const EdgeInsets.only(top: 70),
-                      decoration: const BoxDecoration(
-                        color: Color.fromARGB(255, 185, 223, 114),
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(40),
-                          topRight: Radius.circular(40),
-                        ),
+            ),
+            // CategoryList(),
+            // ignore: prefer_const_constructors
+            // SizedBox(height: 20.0),
+            Expanded(
+              child: Stack(
+                children: <Widget>[
+                  // Our background
+                  Container(
+                    //height: 300,
+                    margin: const EdgeInsets.only(top: 5),
+                    decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 185, 223, 114),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10),
                       ),
                     ),
-                    ListView.builder(
-                      // here we use our demo procuts list
-                      itemCount: products.length,
-                      itemBuilder: (context, index) => ProductCard(
-                        itemIndex: index,
-                        product: products[index],
-                        press: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => DetailsScreen(
-                                product: products[index],
-                              ),
-                            ),
+                  ),
+                  StreamBuilder(
+                      stream: FirebaseFirestore.instance
+                          .collection('catalog-home')
+                          .snapshots(),
+                      builder: (__,
+                          AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>>
+                              snapshot) {
+                        if (snapshot.hasError) {
+                          return const Text(
+                            "eor",
+                            style: TextStyle(
+                                fontSize: 100,
+                                color: Color.fromARGB(255, 226, 7, 7)),
                           );
-                        },
-                      ),
-                    )
-                  ],
-                ),
+                        }
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Text(
+                            "Loading",
+                            style: TextStyle(color: Colors.white),
+                          );
+                        }
+                        // var dataMinuman =
+
+                        final kat = snapshot.requireData;
+                        return ListView.builder(
+                            // here we use our demo procuts list
+                            itemCount: kat.size,
+                            itemBuilder: (context, int index) {
+                              var data = kat.docs[index];
+                              return Container(
+                                margin: const EdgeInsets.symmetric(
+                                  horizontal: 20.0,
+                                  vertical: 20.0 / 2,
+                                ),
+                                // color: Colors.blueAccent,
+                                height: 150,
+                                child: InkWell(
+                                  onTap: () {
+                                    print(kat.docs[index]['name'].toString());
+                                    Get.to(DetailsScreen(), arguments: data);
+                                    // Navigator.push(
+                                    //   context,
+                                    //   MaterialPageRoute(
+                                    //       builder: (context) => DetailsScreen(
+                                    //             product: product,
+                                    //           )),
+                                    // );
+                                  },
+                                  child: Stack(
+                                    alignment: Alignment.bottomCenter,
+                                    children: <Widget>[
+                                      // Those are our background
+                                      Container(
+                                        height: 150,
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                          // color: kBlueColor,
+                                          // boxShadow: const [kDefaultShadow],
+                                        ),
+                                        child: Container(
+                                          margin:
+                                              const EdgeInsets.only(right: 10),
+                                          decoration: BoxDecoration(
+                                            color: Colors.white,
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                          ),
+                                        ),
+                                      ),
+                                      // our product image
+                                      Positioned(
+                                        top: 0,
+                                        right: 0,
+                                        child: Hero(
+                                          tag:
+                                              '${kat.docs[index]['price'].toString()}',
+                                          child: Container(
+                                            padding: const EdgeInsets.symmetric(
+                                                horizontal: kDefaultPadding),
+                                            height: 160,
+                                            // image is square but we add extra 20 + 20 padding thats why width is 200
+                                            width: 150,
+                                            child: Image.asset(
+                                              kat.docs[index]['image']
+                                                  .toString(),
+                                              fit: BoxFit.cover,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      // Product title and price
+                                      Positioned(
+                                        bottom: 0,
+                                        left: 0,
+                                        child: SizedBox(
+                                          height: 136,
+                                          // our image take 200 width, thats why we set out total width - 200
+                                          width: size.width - 200,
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: <Widget>[
+                                              const Spacer(),
+                                              Padding(
+                                                padding:
+                                                    const EdgeInsets.symmetric(
+                                                        horizontal:
+                                                            kDefaultPadding),
+                                                child: Text(
+                                                  kat.docs[index]['name']
+                                                      .toString(),
+                                                  style: Theme.of(context)
+                                                      .textTheme
+                                                      .button,
+                                                ),
+                                              ),
+                                              // it use the available space
+                                              const Spacer(),
+                                              Container(
+                                                padding:
+                                                    const EdgeInsets.symmetric(
+                                                  horizontal: kDefaultPadding *
+                                                      1.5, // 30 padding
+                                                  vertical: kDefaultPadding /
+                                                      4, // 5 top and bottom
+                                                ),
+                                                decoration: const BoxDecoration(
+                                                  color: kSecondaryColor,
+                                                  borderRadius:
+                                                      BorderRadius.only(
+                                                    bottomLeft:
+                                                        Radius.circular(5),
+                                                    topRight:
+                                                        Radius.circular(5),
+                                                  ),
+                                                ),
+                                                child: Text(
+                                                  "\$${kat.docs[index]['price'].toString()}",
+                                                  style: Theme.of(context)
+                                                      .textTheme
+                                                      .button,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              );
+                            });
+                      })
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
@@ -117,8 +258,8 @@ final List<Widget> imageSliders = imgList
                         decoration: const BoxDecoration(
                           gradient: LinearGradient(
                             colors: [
-                              Color.fromARGB(200, 0, 0, 0),
-                              Color.fromARGB(0, 0, 0, 0)
+                              // Color.fromARGB(200, 0, 0, 0),
+                              // Color.fromARGB(0, 0, 0, 0)
                             ],
                             begin: Alignment.bottomCenter,
                             end: Alignment.topCenter,
